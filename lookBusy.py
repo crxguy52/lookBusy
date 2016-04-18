1f25652e2db6cbcf13ccb591f6557e6a8c3f9f8e
crxguy52 <victorzaccardo@gmail.com>
2016-04-18 08:45:10 -0400

    made it look nicer

--- lookBusy.py
+++ lookBusy.py
@@ -3,7 +3,7 @@ import time
 
 #Prints jibberish to make it look like cool things are happening
 
-runtime = 10     #seconds
+runtime = 20     #seconds
 linemin = 7      #words
 linemax = 10     #words
 wordmin = 4      #chars
@@ -14,17 +14,23 @@ lines = int(runtime/delaytime)
 #only use lowercase letters
 letters = 'abcdefghijklmnopqrstuvwxyz'
 
-#print some lines
+#generate a bunch of lines
 for i in range(lines):
-    #print some words
+    #generate one line
     line = ''
+    
+    #generate words for one line
     for j in range(randrange(wordmin,wordmax)):
-        #build a word and print it
         word = ''
+        
+        #build one word and add it to the line        
         for k in range(randrange(wordmin, wordmax)):
             word += choice(letters)
         line += word + ' '
+        
+    #print the line
     print(line)
-    #Wait a random time between 0 and 2*delaytime
+    
+    #Wait a random time between 0 and 2*delaytime, then loop
     dly = randrange(0, int(2*delaytime*10000))/10000.0
     time.sleep(dly)
